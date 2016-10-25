b3ad7eb5ce7070ffcf01f3727a80b2e8d81e9d35
arnaud droxler <arnaud.droxler@gmail.com>
2016-10-25 10:23:39 +0200

    premier commit

---
+++ app/src/main/java/he_arc/marble_mazze/MazzeActivity.java
@@ -0,0 +1,13 @@
+package he_arc.marble_mazze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MazzeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mazze);
+    }
+}
